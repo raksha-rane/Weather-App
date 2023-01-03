0c6bfbb15007fab908e0c45aff76c4e0cbe89887
Raksha Rane <raksharane18@gmail.com>
2023-01-03 19:28:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f86baaf1f6msha10cbc6cd2eba16p159c2djsn57a7003bba34',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city) => {
+	cityName.innerHTML = city
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then(response => response.json())
+		.then((response) => {
+
+			console.log(response)
+			cloud_pct.innerHTML = response.cloud_pct
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			max_temp.innerHTML = response.max_temp
+			min_temp.innerHTML = response.min_temp
+			sunrise.innerHTML = response.sunrise
+			sunset.innerHTML = response.sunset
+			temp.innerHTML = response.temp
+			wind_degrees.innerHTML = response.wind_degrees
+			wind_speed.innerHTML = response.wind_speed
+		})
+		.catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e) => {
+	e.preventDefault()
+	getWeather(city.value)
+})
+
+getWeather("Mumbai")
